e118e575d5ad0f8806c2ea9a9815955b0f3f37ac
sourav-mahmud <souravmahmud32@gmail.com>
2022-10-27 17:37:01 +0600

    courses data updated

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
-app.get('/', (req, res) => {
-    res.send('News API Running');
+app.get("/", (req, res) => {
+  res.send("News API Running");
 });
 
-app.get('/categories', (req, res) =>{
-    res.send(categories)
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(courses);
+  } 
+  else {
+    const category_courses = courses.filter((c) => c.category_id === id);
+    res.send(category_courses);
+  }
+});
+
+app.get('/courses', (req, res) =>{
+    res.send(courses);
 })
 
-app.listen(port, () =>{
-    console.log('CourseGuru server running on port ', port);
-})
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((c) => c._id === id);
+  res.send(selectedCourse);
+});
+
+app.listen(port, () => {
+  console.log("CourseGuru server running on port ", port);
+});
