c4b7b0eea0658e588e6049cdc3ad9e02869ebeb0
sourav-mahmud <souravmahmud32@gmail.com>
2022-10-28 13:38:26 +0600

    vercel data updated

--- index.js
+++ index.js
@@ -20,16 +20,15 @@ app.get("/category/:id", (req, res) => {
   const id = req.params.id;
   if (id === "08") {
     res.send(courses);
-  } 
-  else {
+  } else {
     const category_courses = courses.filter((c) => c.category_id === id);
     res.send(category_courses);
   }
 });
 
-app.get('/courses', (req, res) =>{
-    res.send(courses);
-})
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
 app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
